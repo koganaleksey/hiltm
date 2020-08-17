6a1c06cce7f4499c6e1cdd1d8ecf5a99438c2507
Kogan Alexey <koganaleksey@mail.ru>
2020-08-17 18:16:46 +0530

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -1,11 +1,11 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "koganaleksey@mail.ru; // this is your Email address
+    $to = "koganaleksey@mail.ru"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
     $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
@@ -16,4 +16,3 @@ if(isset($_POST['submit'])){
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
-?>
