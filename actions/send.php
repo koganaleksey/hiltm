efb246e4bf391c3473ce1ccc7b09964ee91f93d6
Kogan Alexey <koganaleksey@mail.ru>
2020-08-17 18:49:33 +0530

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -1,11 +1,12 @@
 <?php 
 $ToEmail = 'koganaleksey@mail.ru'; 
-$EmailSubject = 'Site contact form'; 
+$EmailSubject = 'Сообщение из формы сайта'; 
 $mailheader = "From: ".$_POST["email"]."\r\n"; 
 $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
 $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-$MESSAGE_BODY = "Name: ".$_POST["name"].""; 
-$MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+$MESSAGE_BODY = "Name: ".$_POST["name"]."\n\n"; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"]."\n\n"; 
 $MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
-mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
+header('Location: index.html')
 ?>
