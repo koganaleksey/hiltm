92374a5aa1c9a9e1a5571c290cc5bb24c1153dcc
Kogan Alexey <koganaleksey@mail.ru>
2020-08-17 19:31:07 +0530

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -1,13 +1,13 @@
 <?php
+require('actions/Validator.php');
+
 if(isset($_POST['email'])) {
   $email_to = "koganaleksey@mail.ru";
-  $email_subject = "website form submission";
+  $email_subject = "Отправка формы на сайте";
 
   function died($error) {
-    echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-    echo "These errors appear below.<br /><br />";
+    echo "Нам очень жаль, но в отправленной вами форме были обнаружены ошибки. ";
     echo $error."<br /><br />";
-    echo "Please go back and fix these errors.<br /><br />";
     die();
   }
 
@@ -16,7 +16,7 @@ if(isset($_POST['email'])) {
   if (!isset($_POST['name']) ||
     !isset($_POST['email']) ||
     !isset($_POST['message'])) {
-    died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    died('Сожалеем, но, похоже, возникла проблема с отправленной вами формой.');       
   }
 
   $name = $_POST['name'];
@@ -34,9 +34,9 @@ if(isset($_POST['email'])) {
     return str_replace($bad,"",$string);
   }
 
-  $email_message .= "Name: ".clean_string($name)."\n";
+  $email_message .= "Имя: ".clean_string($name)."\n";
   $email_message .= "Email: ".clean_string($email_from)."\n";
-  $email_message .= "Message: ".clean_string($message)."\n";
+  $email_message .= "Сообщение: ".clean_string($message)."\n";
 
   $headers = 'From: '.$email_from."\r\n" .
              'Reply-To: '.$email_from."\r\n" .
@@ -44,7 +44,7 @@ if(isset($_POST['email'])) {
   @mail($email_to, $email_subject, $email_message, $headers);  
 ?>
 
-<div class="d-flex justify-content-center align-items-center">
+<div class="d-flex justify-content-center align-items-center" style="width:100wv;height:100vh">
   <p style="width:400px">Thank you for contacting us. We will be in touch with you soon. You will now be redirected back to example.com.</p></div>
 <META http-equiv="refresh" content="2;URL=http://hiltm.com">
 
