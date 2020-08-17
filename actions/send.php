71de52c4da2e85065620dff2aa2a99a7d11c613e
Kogan Alexey <koganaleksey@mail.ru>
2020-08-17 19:00:49 +0530

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -1,12 +1,53 @@
-<?php 
-$ToEmail = 'koganaleksey@mail.ru'; 
-$EmailSubject = 'Сообщение из формы сайта'; 
-$mailheader = "From: ".$_POST["email"]."\r\n"; 
-$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
-$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-$MESSAGE_BODY = "Name: ".$_POST["name"]."\n\n"; 
-$MESSAGE_BODY .= "Email: ".$_POST["email"]."\n\n"; 
-$MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
-mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
-header('Location: index.html')
+<?php
+if(isset($_POST['email'])) {
+  $email_to = "myemail@example.com";
+  $email_subject = "website form submission";
+
+  function died($error) {
+    echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+    echo "These errors appear below.<br /><br />";
+    echo $error."<br /><br />";
+    echo "Please go back and fix these errors.<br /><br />";
+    die();
+  }
+
+  if(!empty($_POST['website'])) die();
+
+  if (!isset($_POST['name']) ||
+    !isset($_POST['email']) ||
+    !isset($_POST['message'])) {
+    died('We are sorry, but there appears to be a problem with the form you submitted.');       
+  }
+
+  $name = $_POST['name'];
+  $email_from = $_POST['email'];
+  $message = $_POST['message'];
+
+  $error_message = "";
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+  $email_message = "Form details below.\n\n";
+
+  function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+  }
+
+  $email_message .= "Name: ".clean_string($name)."\n";
+  $email_message .= "Email: ".clean_string($email_from)."\n";
+  $email_message .= "Message: ".clean_string($message)."\n";
+
+  $headers = 'From: '.$email_from."\r\n" .
+             'Reply-To: '.$email_from."\r\n" .
+             'X-Mailer: PHP/' . phpversion();
+  @mail($email_to, $email_subject, $email_message, $headers);  
+?>
+
+Thank you for contacting us. We will be in touch with you soon. You will now be redirected back to example.com.
+<META http-equiv="refresh" content="2;URL=http://hiltm.com">
+
+<?php
+}
+die();
 ?>
