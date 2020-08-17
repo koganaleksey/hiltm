2b36e01e7370b778a0590ec50402285228c687b3
Kogan Alexey <koganaleksey@mail.ru>
2020-08-17 18:40:49 +0530

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -1,53 +1,11 @@
-<?php
-require('actions/ErrorHandler.php');
-require('actions/Validator.php');
-require('actions/Session.php');
-
-$errorHandler = new ErrorHandler;
-
-
-    if (!empty($_POST)) {
-            $validator = new Validator($errorHandler);
-            $validation = $validator->check($_POST, [
-      				'name' => [
-      					'required' => true,
-      					'maxlength' => 20,
-      					'minlength' => 3
-      				],
-      				'email' => [
-      					'required' => true,
-      					'maxlength' => 20,
-      					'minlength' => 3,
-      					'email' => true
-      				],
-      				'message' => [
-      					'required' => true,
-      					'minlength' => 6,
-      					'maxlength' => 900
-      				]
-
-        	]);
-
-            if ($validator->fails()) {
-                $errors = $validator->errors()->all();
-                //header('Location: '. '../feedback.php');
-				      } else {
-                
-                $to      = 'koganaleksey@mail.ru';
-                $subject = $_POST['name'];
-                $message = $_POST['message'];
-                $headers = array(
-                          'From' => $_POST['email'],
-                          // 'Reply-To' => 'ikoganaleksey@yandex.ru',
-                          );
-    
-                $mail = mail($to, $subject, $message, implode("\r\n", $headers));
-
-                if($mail){
-                  Session::flash('success', 'Сообщение успешно отправлено');
-                  
-                }
-
-              }
-}
+<?php 
+$ToEmail = 'koganaleksey@mail.ru'; 
+$EmailSubject = 'Site contact form'; 
+$mailheader = "From: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["name"].""; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+$MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
 ?>
