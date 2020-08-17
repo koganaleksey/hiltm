3b9f5987efb16ddc3d75caa8261ef3808bc2c5aa
Kogan Alexey <koganaleksey@mail.ru>
2020-08-17 17:40:47 +0530

    Automated push via gulp

---
+++ actions/send.php
@@ -0,0 +1,53 @@
+<?php
+require('actions/ErrorHandler.php');
+require('actions/Validator.php');
+require('actions/Session.php');
+
+$errorHandler = new ErrorHandler;
+
+
+    if (!empty($_POST)) {
+            $validator = new Validator($errorHandler);
+            $validation = $validator->check($_POST, [
+      				'name' => [
+      					'required' => true,
+      					'maxlength' => 20,
+      					'minlength' => 3
+      				],
+      				'email' => [
+      					'required' => true,
+      					'maxlength' => 20,
+      					'minlength' => 3,
+      					'email' => true
+      				],
+      				'message' => [
+      					'required' => true,
+      					'minlength' => 6,
+      					'maxlength' => 900
+      				]
+
+        	]);
+
+            if ($validator->fails()) {
+                $errors = $validator->errors()->all();
+                //header('Location: '. '../feedback.php');
+				      } else {
+                
+                $to      = 'koganaleksey@mail.ru';
+                $subject = $_POST['name'];
+                $message = $_POST['message'];
+                $headers = array(
+                          'From' => $_POST['email'],
+                          // 'Reply-To' => 'ikoganaleksey@yandex.ru',
+                          );
+    
+                $mail = mail($to, $subject, $message, implode("\r\n", $headers));
+
+                if($mail){
+                  Session::flash('success', 'Сообщение успешно отправлено');
+                  
+                }
+
+              }
+}
+?>
