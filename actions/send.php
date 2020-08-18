bda7bd2057d70108e44466a4cd9c38714e0bb3eb
Kogan Alexey <koganaleksey@mail.ru>
2020-08-18 14:38:03 +0530

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -1,9 +1,14 @@
 <?php
+
+$name = $_POST['name'];
+$email_from = $_POST['email'];
+$message = $_POST['message'];
+
 $mail = array(
   'to' => "koganaleksey@mail.ru",
-  'subject' => "Новое сообщение",
-  'message' => "<html><body><p>Текст сообщения</p></body></html>",
-  'headers' => "MIME-Version: 1.0\r\n" . "Content-type: text/html; charset=utf-8\r\n" . "From: Эта кириллица выводится иероглифами <frommail@site.ru>\r\n"
+  'subject' => "Сообщение с сайта hiltm.com",
+  'message' => "Имя: " . $name . "\n\n" . "Email: " . $email_from . "\n\n" . "Сообщение: " . $message,
+  'headers' => "MIME-Version: 1.0\r\n" . "Content-type: text/plain; charset=utf-8\r\n" . "From: <hiltm.com>\r\n"
 );
 
 mail($mail['to'], $mail['subject'], $mail['message'], iconv('utf-8', 'windows-1251', $mail['headers']));
