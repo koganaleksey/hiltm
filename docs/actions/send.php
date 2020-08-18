0ecf65c161c85634c84d54ef09907bdbaa1eaa41
Kogan Alexey <koganaleksey@mail.ru>
2020-08-18 16:02:20 +0530

    Версия 1.0

--- docs/actions/send.php
+++ docs/actions/send.php
@@ -1,73 +1,32 @@
 <?php
 
-if (isset($_POST['email'])) {
-  $email_to = "info@hiltm.com";
-  $email_subject = "Отправка формы c сайта hiltm.com";
+if (!empty($_POST['website'])) die();
 
-  function died($error)
-  {
-    echo "Нам очень жаль, но в отправленной вами форме были обнаружены ошибки. ";
-    echo $error . "<br /><br />";
-    die();
-  }
+$name = $_POST['name'];
+$email_from = $_POST['email'];
+$message = $_POST['message'];
+$success_message = '<div class="d-flex justify-content-center align-items-center text-center" style="width:100vw;height:100vh">
+<h3 style="width:400px;"><span class="font-weight-bold text-success">Спасибо, что связались с нами.</span><br><br><span class="font-weight-light">Мы свяжемся с вами в ближайшее время.<br>Теперь вы будете перенаправлены обратно на <a href="https://hiltm.com">hiltm.com.</a></span></h3>
+</div>';
 
-  if (!empty($_POST['website'])) die();
+$mail = array(
+  'to' => "info@hiltm.com",
+  'subject' => "Сообщение с сайта hiltm.com",
+  'message' => "Имя: " . $name . "\n\n" . "Email: " . $email_from . "\n\n" . "Сообщение: " . "\r\n" . $message,
+  'headers' => "MIME-Version: 1.0\r\n" . "Content-type: text/plain; charset=utf-8\r\n" . "From: <hiltm.com>\r\n"
+);
 
-  if (
-    !isset($_POST['name']) ||
-    !isset($_POST['email']) ||
-    !isset($_POST['message'])
-  ) {
-    died('Сожалеем, но, похоже, возникла проблема с отправленной вами формой.');
-  }
+mail($mail['to'], $mail['subject'], $mail['message'], iconv('utf-8', 'windows-1251', $mail['headers']));
 
-  $name = $_POST['name'];
-  $email_from = $_POST['email'];
-  $message = $_POST['message'];
+echo iconv('utf-8', 'windows-1251', $success_message);
 
-  $error_message = "";
-  if (strlen($error_message) > 0) {
-    died($error_message);
-  }
-  $email_message = "Детали формы ниже.\n\n";
-
-  function clean_string($string)
-  {
-    $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-    return str_replace($bad, "", $string);
-  }
-
-  $email_message .= "Имя: " . clean_string($name) . "\n\n";
-  $email_message .= "Email: " . clean_string($email_from) . "\n\n";
-  $email_message .= "Сообщение: " . clean_string($message);
-
-  $headers = 'From: ' . $email_from . "\r\n" .
-    'Reply-To: ' . $email_from . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-  @mail($email_to, $email_subject, $email_message, $headers);
 ?>
 
-  <!DOCTYPE html>
-  <html lang="ru">
-
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="refresh" content="6;URL=https://hiltm.com">
-
-    <link rel="stylesheet" href="../assets/css/main.min.css">
-  </head>
-
-  <body>
-
-    <div class="d-flex justify-content-center align-items-center text-center" style="width:100vw;height:100vh">
-      <h3 style="width:400px;"><span class="font-weight-bold text-success">Спасибо, что связались с нами.</span><br><br><span class="font-weight-light">Мы свяжемся с вами в ближайшее время.<br>Теперь вы будете перенаправлены обратно на <a href="hiltm.com">hiltm.com.</a></span></h3>
-    </div>
-
-  </body>
+<head>
+  <meta http-equiv="refresh" content="7;URL=https://hiltm.com">
+  <link rel="stylesheet" href="../assets/css/main.min.css">
+</head>
 
 <?php
-}
 die();
 ?>
